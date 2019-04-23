71e2efcb4a8dd1afb897b50dd08017c0abe4fbce
Declan <declan.gundrum.17@gmail.com>
2019-04-23 16:17:52 -0500

    HAI

--- src/QuizGenerator/createNewQuestionScreen.java
+++ src/QuizGenerator/createNewQuestionScreen.java
@@ -1,8 +1,17 @@
 package QuizGenerator;
 
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.awt.*;
+
 public class createNewQuestionScreen {
 
   public Question show() {
+    Stage main = new Stage();
+    BorderPane screen = new BorderPane();
+    VBox fields = new VBox();
 
     return null;
   }
