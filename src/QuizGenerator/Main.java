7357a65092b73ebab97976d9b30a4129853b0abc
Justin <jdburns2@wisc.edu>
2019-04-24 01:10:14 -0500

    Yee haw, Anieay is buttiful

--- src/QuizGenerator/Main.java
+++ src/QuizGenerator/Main.java
@@ -1,63 +1,69 @@
 package QuizGenerator;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
 
 
 public class Main extends Application {
 
-  BorderPane border = new BorderPane();
-  ListView<Quiz> quizzes = new ListView<>();
+  BorderPane screen = new BorderPane();
+  ArrayList<BorderPane> pages= new ArrayList<>();
+  ArrayList<BorderPane> quizPages= new ArrayList<>();
+  Scene currentScene = new Scene(screen, 300, 275);
+
+  ListView<Button> quizzes = new ListView<>();
+
 
   @Override
   public void start(Stage primaryStage) throws Exception {
-
-    border.setStyle("-fx-background-color: #fa8072;");
-
-
-    TextField title = new TextField();
-    title.setText("CS400 Quiz Generator");
-    title.setStyle("-fx-background-color: #fa8072;");
-    border.setTop(title);
-    //gridPane.setAlignment(Pos.TOP_CENTER);
-
-
-    TextField title2 = new TextField();
-    title.setText("Declan gay - Hi atessa");
-    title.setStyle("-fx-background-color: #fa8072;");
-    //gridPane.setAlignment(Pos.CENTER);
-
-    Button newQuiz = new Button("New Quiz");
-    newQuiz.setOnAction(event -> {
-      makeQuiz current = new makeQuiz();
-      quizzes.getItems().add(current.runner());
-    });
-    border.setRight(newQuiz);
-
-    border.setCenter(quizzes);
-
-
-    primaryStage.setScene(new Scene(border, 300, 275));
-
-
+    setHomePage(primaryStage);
+    primaryStage.setScene(currentScene);
     primaryStage.show();
   }
-
-  public void setupQuizzes() {
-    quizzes.setMaxSize(200, 200);
-    quizzes.setStyle("-fx-background-color: #fa8072;");
-  }
-
 
   public static void main(String[] args) {
     launch(args);
   }
+
+  public void setHomePage(Stage primaryStage){
+    TextField title = new TextField("CS400 Quiz Generator");
+    Button addNewQuiz = new Button("Add New Quiz");
+
+    screen.setStyle("-fx-background-color: #FA8072;");
+    quizzes.setStyle("-fx-background-color: #FA8072;");
+    addNewQuiz.setStyle("-fx-background-color: #FA8072;");
+    title.setStyle("-fx-background-color: #FA8072;");
+    title.setFont(Font.font(40));
+    screen.setTop(title);
+    screen.setCenter(quizzes);
+    quizzes.getItems().add(addNewQuiz);
+    primaryStage.setScene(currentScene);
+    primaryStage.show();
+
+
+
+    addNewQuiz.setOnAction(event -> {
+      Button quiz= new Button();
+      quiz.setOnAction(event1 -> {
+
+
+
+
+
+
+
+
+      });
+      quizzes.getItems().add(quiz);
+    });
+  }
+
 }
