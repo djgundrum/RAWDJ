0e0d21d8a5fe910f2c64db10abda6ac18fb85404
atessaamjadi <aamjadi@wisc.edu>
2019-05-02 17:58:12 -0500

    hi

--- src/QuizGenerator/Main.java
+++ src/QuizGenerator/Main.java
@@ -53,13 +53,16 @@ public class Main extends Application {
     // This is the button on the top below the title string
     Button addNewQuiz = new Button("Add New Quiz");
 
+    //This is a button to exit
+    Button exit = new Button("Exit");
+
     // This is a button so that the user can load a previous quiz generator file
     Button loadFile = new Button("Load File");
 
     /// This is a button to save all the current quizzes that the user has made
     Button saveFile = new Button("Save File");
 
-    topper.getChildren().addAll(addNewQuiz, loadFile, saveFile);
+    topper.getChildren().addAll(addNewQuiz, loadFile, saveFile, exit);
 
     // Screen is the main border pane for this scene
     screen.setStyle("-fx-background-color: #E0FFFF;");
@@ -67,6 +70,7 @@ public class Main extends Application {
     // Quizzes is the listview in the middle of the screen
     quizzes.setStyle("-fx-background-color: #E0FFFF;");
     addNewQuiz.setStyle("-fx-background-color: #00CED1;-fx-border-color: black;");
+    exit.setStyle("-fx-background-color: #00CED1;-fx-border-color: black;");
     loadFile.setStyle("-fx-background-color: #00CED1;-fx-border-color: black;");
     saveFile.setStyle("-fx-background-color: #00CED1;-fx-border-color: black;");
     title.setStyle("-fx-background-color: #E0FFFF;");
@@ -83,6 +87,13 @@ public class Main extends Application {
     // Shows the current screen
     primaryStage.show();
 
+    //lets user exit
+    exit.setOnAction(event -> {
+      closeHelper(fileChooser, primaryStage);
+    });
+
+
+
     // Sets what the addquiz button will do
     // Calls the makeQuiz class
     addNewQuiz.setOnAction(event -> {
@@ -97,6 +108,45 @@ public class Main extends Application {
     saveFile.setOnAction(event -> {
       saveHelper(fileChooser, primaryStage);
     });
+  }
+
+  private void closeHelper(FileChooser fileChooser, Stage primaryStage){
+
+    Stage stage = new Stage();
+
+    BorderPane screen = new BorderPane();
+    Scene currentScene = new Scene(screen, 400, 200);
+
+    // Sets the string at the top of the current scene
+    Label title = new Label("Do you want to exit");
+
+    screen.setTop(title);
+
+    HBox box = new HBox();
+
+    Button save = new Button("Save and exit");
+    Button noSave = new Button("Exit without saving");
+
+    box.getChildren().addAll(save, noSave);
+    screen.setCenter(box);
+
+    save.setOnAction(event -> {
+      saveHelper(fileChooser, primaryStage);
+      stage.close();
+      primaryStage.close();
+    });
+
+    noSave.setOnAction(event -> {
+      stage.close();
+      primaryStage.close();
+    });
+
+    stage.setScene(currentScene);
+
+    stage.show();
+
+
+
   }
 
   @SuppressWarnings("unchecked")
